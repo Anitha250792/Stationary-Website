4544eb42b6d3dd02a5a78c6eb90d2fc84121bee2
Akila <akila271819@gmail.com>
2025-09-01 23:28:12 +0530

    local host

--- supplyhive/settings.py
+++ supplyhive/settings.py
@@ -5,7 +5,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'dev-secret-key-change-in-prod'
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'stationary-website-2.onrender.com']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
